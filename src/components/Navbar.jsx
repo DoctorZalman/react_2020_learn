5983ae3353d45be2d11745b4a66d263881af5eb7
doctorzalman <georgesun30@gmail.com>
2020-11-28 19:44:37 +0200

    14_les

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+	return <header className="header">
+		<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSS-uH0ttxOHBdtpV-G3iokQRz1bRVZu5dQfg&usqp=CAU" alt=""/>
+	</header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+	return <nav className="nav">
+		<div>
+			<a>Profile</a>
+			<a>Messages</a>
+			<a>News</a>
+			<a>Music</a>
+			<a>Settings</a>
+		</div>
+	</nav>
+}
+
+export default Navbar;
