578b162b136c533952299c38475e9870108c05af
doctorzalman <georgesun30@gmail.com>
2020-12-22 10:23:35 +0200

    33_less_done

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,5 @@
+import {rerenderEntireTree} from "../render";
+
 let state = {
 	profilePage: {
 		posts: [
@@ -28,8 +30,9 @@ export let addPost = (postMessage) => {
 		id: 5,
 		message: postMessage,
 		likesCount: 0
-	}
+	};
 	state.profilePage.posts.push(newPost);
+	rerenderEntireTree(state);
 }
 
 export default state;
