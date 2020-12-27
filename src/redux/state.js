fbc086f99c981b1b4f059fe55ad4d8670f3bd050
doctorzalman <georgesun30@gmail.com>
2020-12-27 11:06:16 +0200

    37_less_done

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,53 +1,53 @@
-// import {rerenderEntireTree} from "../render";
-let rerenderEntireTree = () => {
-	console.log('State changed')
-}
-let state = {
-	profilePage: {
-		posts: [
-			{id: 1, message: "How is your cat", likesCount: 12},
-			{id: 2, message: "Hi", likesCount: 13},
-			{id: 3, message: "Good", likesCount: 14},
-			{id: 4, message: "Nice", likesCount: 15}
-		],
-		newPostText: 'it'
+let store = {
+	_state: {
+		profilePage: {
+			posts: [
+				{id: 1, message: "How is your cat", likesCount: 12},
+				{id: 2, message: "Hi", likesCount: 13},
+				{id: 3, message: "Good", likesCount: 14},
+				{id: 4, message: "Nice", likesCount: 15}
+			],
+			newPostText: 'it'
+		},
+		dialogsPage: {
+			dialogs: [
+				{id:1, name: "Roma"},
+				{id:4, name: "Vasia"},
+				{id:2, name: "Vika"},
+				{id:3, name: "Endry"}
+			],
+			messages: [
+				{id: 1, message: "How is your cat", likesCount: 12},
+				{id: 2, message: "Hi", likesCount: 13},
+				{id: 3, message: "Good", likesCount: 14},
+				{id: 4, message: "Nice", likesCount: 15}
+			]
+		}
 	},
-	dialogsPage: {
-		dialogs: [
-			{id:1, name: "Roma"},
-			{id:4, name: "Vasia"},
-			{id:2, name: "Vika"},
-			{id:3, name: "Endry"}
-		],
-		messages: [
-			{id: 1, message: "How is your cat", likesCount: 12},
-			{id: 2, message: "Hi", likesCount: 13},
-			{id: 3, message: "Good", likesCount: 14},
-			{id: 4, message: "Nice", likesCount: 15}
-		]
+	getState() {
+		return this._state;
+	},
+	rerenderEntireTree () {
+		console.log('State changed')
+	},
+	addPost () {
+		let newPost ={
+			id: 5,
+			message: this._state.profilePage.newPostText,
+			likesCount: 0
+		};
+		this._state.profilePage.posts.push(newPost);
+		this._state.profilePage.newPostText = '';
+		this._rerenderEntireTree(this._state);
+	},
+	updateNewPostText (newText)  { // обновляємо state даними, які приходять з поля вводу поста
+		this._state.profilePage.newPostText = newText;
+		this._rerenderEntireTree(this._state);
+	},
+	subscribe (observer) {
+		this._rerenderEntireTree = observer;
 	}
 }
 
-window.state = state; // відображення у консолі зміни у state
-
-export const addPost = () => {
-	let newPost ={
-		id: 5,
-		message: state.profilePage.newPostText,
-		likesCount: 0
-	};
-	state.profilePage.posts.push(newPost);
-	state.profilePage.newPostText = '';
-	rerenderEntireTree(state);
-}
-
-export const updateNewPostText = (newText) => { // обновляємо state даними, які приходять з поля вводу поста
-	state.profilePage.newPostText = newText;
-	rerenderEntireTree(state);
-}
-
-export const subscribe = (observer) => {
-	rerenderEntireTree = observer;
-}
-
-export default state;
+export default store;
+window.store = store; // відображення у консолі зміни у state
