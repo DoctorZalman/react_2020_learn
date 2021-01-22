4507427e97081cfa3b2ef67ff3e957a51d712e21
doctorzalman <georgesun30@gmail.com>
2021-01-22 17:28:16 +0200

    61_less_done-cookie-login-auth_me

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import s from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
 	return <header className={s.header}>
 		<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSS-uH0ttxOHBdtpV-G3iokQRz1bRVZu5dQfg&usqp=CAU" alt=""/>
+		<div className={s.loginBlock}>
+			{ props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink> }
+		</div>
 	</header>
 }
 
