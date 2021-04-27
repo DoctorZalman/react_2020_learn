513d05932b1262d6a5c025994c82af1831e1e5d9
doctorzalman <georgesun30@gmail.com>
2021-04-27 11:52:38 +0300

    redux-ducks refactoring

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import SamuraiApp from "./App";
+import App from "./App";
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import store from "./redux/redux-store";
 
 ReactDOM.render(
-	<SamuraiApp/>,
-	document.getElementById('root')
+	<BrowserRouter>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</BrowserRouter>, document.getElementById('root')
 );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
